879294467b96005bea4ff2146bdc375260623326
yousuf siddiqui <yousufsiddiqui747@gmail.com>
2020-07-23 16:51:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,125 @@
+
+// === Chapter #1 Task 1
+
+// alert ("Error ! Piz Enter Your alid Code")
+
+// === chapter #1 task 2 
+
+// alert("Welcom To Js Land \n In Happing Codeing");
+
+// === chapter #1 task 4 
+
+
+// alert("Hello world")
+// alert("Good bye World")
+
+
+// === chapter #1 task 5 //
+
+// var y = ("Hello World. Hope You Doing Good")
+// console.log (y)
+
+// === chapter #2 task 4 //
+
+// alert("yousuf")
+// alert("15 Years old")
+// alert("Certified Mobile Application Development")
+
+// === chapter #2 task 1 //
+ 
+// var = "username"
+
+// var=email "(e.g. example@example.com)."
+
+// function showImage(e){
+  //  var modalImage = document.getElementById("modalImage");
+   // modalImage.src = e.src  
+//}
+
+// === chapter #2 task 6 //
+
+// var x = "My Email Adress is yousud@hotmail.com"
+// alert (x);
+
+// === chapter #2 task 7 //
+
+// var book = "A Smart Way To Run"
+// alert (book);
+
+// === chapter #2 task 8 //
+
+// var x = "Hello World.. I Can Write HTML"
+// document.write (x)
+
+// === chapter #2 task 9 //
+
+// var b = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert (b);
+
+// === chapter #3 task 1 //
+
+// var age = "1 am 18 year old"
+// alert (age)
+
+// === chapter #3 task 3 //
+
+// var x = "My Birth Is 2002"
+// document.write (x)
+
+// === chapter #3 task 4 //
+
+// var i = " Yousuf order 2 shirt on draz Closing Store"
+// document.write (i)
+
+// === chapter #4 task 1 //
+ 
+//var a = var b = 0;
+
+// === chapter #4 task 2 //
+
+
+
+
+// === chapter #5 Question 1 //
+
+// var x = "15";
+// var y = "5";
+// alert (x+y);
+
+// var x = "15";
+// var y = "5";
+// alert (x-y);
+
+// var x = "15";
+// var y = "5";
+// alert (x*y);
+
+// var x = "15";
+// var y = "5";
+// alert (x%y);
+
+
+// === chapter #5 Question 5 //
+
+// document.write("5" + "x" + "1" + "=" + 5*1 + "<br>")
+// document.write("5" + "x" + "2" + "=" + 5*2 + "<br>")
+// document.write("5" + "x" + "3" + "=" + 5*3 + "<br>")
+// document.write("5" + "x" + "4" + "=" + 5*4 + "<br>")
+// document.write("5" + "x" + "5" + "=" + 5*5 + "<br>")
+// document.write("5" + "x" + "6" + "=" + 5*6 + "<br>")
+// document.write("5" + "x" + "7" + "=" + 5*7 + "<br>")
+// document.write("5" + "x" + "8" + "=" + 5*8 + "<br>")
+// document.write("5" + "x" + "9" + "=" + 5*9 + "<br>")
+// document.write("5" + "x" + "10" + "=" + 5*10 + "<br>")
+
+
+
+
+
+
+
+
+
+
+
+
